183d83b250ae9f3e4931de920631f5110d8e25df
Oleksii Ponochevnyi <oleksii_ponochevnyi@epam.com>
2022-01-12 12:31:59 +0200

    [EDP-DDM-9229] Spring Boot 2.6.1. Cloud 2021.0.0
    Change-Id: Ia363dcaff1b72b5b8778ad15809fd40bc77b8ff9

--- src/main/resources/META-INF/resources/webjars/camunda/app/admin/scripts/config.js
+++ src/main/resources/META-INF/resources/webjars/camunda/app/admin/scripts/config.js
@@ -1,9 +1,5 @@
-window.camAdminConf = {
-  customScripts: {
-    ngDeps: ['custom-logout'],
-    deps: ['jquery', 'custom-logout'],
-    paths: {
-      'custom-logout': '../logout'
-    }
-  }
+export default {
+  customScripts: [
+    'custom/logout'
+  ]
 };
---
+++ src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/custom/logout.js
@@ -0,0 +1,20 @@
+let observer = new MutationObserver(() => {
+  const logoutButton = document.querySelectorAll(".logout > a")[0];
+  if (logoutButton) {
+    var parent = logoutButton.parentElement
+    parent.removeChild(logoutButton)
+    var newLogout = document.createElement('a');
+    newLogout.setAttribute('className', 'ng-binding')
+    newLogout.innerText = logoutButton.innerText.replaceAll('\n', '');
+    newLogout.setAttribute('href', 'logout');
+    parent.appendChild(newLogout)
+    observer.disconnect();
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false
+});
